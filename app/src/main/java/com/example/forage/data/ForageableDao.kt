72f838b584d76e1e48620420f9f838648d104dbd
TimChen <fatfattim@gmail.com>
2022-05-20 11:48:51 +0800

    To add the implementation of ROOM's data and Dao

--- app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -15,22 +15,29 @@
  */
 package com.example.forage.data
 
-import androidx.room.Dao
+import android.content.ClipData
+import androidx.room.*
+import com.example.forage.model.Forageable
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for database interaction.
  */
 @Dao
 interface ForageableDao {
+    @Query("SELECT * FROM forageable")
+    fun getForageables(): Flow<List<Forageable>>
 
-    // TODO: implement a method to retrieve all Forageables from the database
+    @Query("SELECT * FROM forageable WHERE id = :id")
+    fun getForageable(id: Long): Flow<Forageable>
 
-    // TODO: implement a method to retrieve a Forageable from the database by id
+    // The OnConflictStrategy.IGNORE strategy REPLACE a new item if it's primary key is already in the database.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(forageable: Forageable)
 
-    // TODO: implement a method to insert a Forageable into the database
-    //  (use OnConflictStrategy.REPLACE)
+    @Update
+    suspend fun update(forageable: Forageable)
 
-    // TODO: implement a method to update a Forageable that is already in the database
-
-    // TODO: implement a method to delete a Forageable from the database.
+    @Delete
+    suspend fun delete(forageable: Forageable)
 }
